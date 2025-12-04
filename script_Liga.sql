01018d8ac161be9d4af7e1e85f6d5f712286087f
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-12-04 21:19:26 +0100

    finished liga

--- script_Liga.sql
+++ script_Liga.sql
@@ -1,7 +1,7 @@
 use liga_db;
 
 create table equipos
-	(id_equipos int unsigned auto_increment primary key,
+	(id_equipo int unsigned auto_increment primary key,
 	nombre varchar(30) not null unique,
     estadio varchar(30) not null,
     aforo int default 2000 ,
@@ -13,7 +13,8 @@ create table jugadores
 	(id_jugador int unsigned auto_increment primary key,
     nombre varchar(30) not null unique,
     fecha_nac date not null,
-    id_equipo int not null);
+    id_equipo int not null,
+    constraint fk_equipos_jugadores foreign key (id_equipo) references equipos(id_equipo));
 
 create table partidos
 	(id_equipo_casa int unsigned,
@@ -22,7 +23,16 @@ create table partidos
     goles_casa int not null default -1,
     goles_fuera int not null default -1,
     observaciones varchar(500),
-    );
+    constraint pk_id_casa_id_fuera primary key (id_equipo_casa, id_equipo_fuera),
+    constraint fk_id_casa foreign key (id_equipo_casa) references equipos(id_equipo),
+    constraint fk_id_fuera foreign key (id_equipo_fuera) references equipos(id_equipo));
     
 create table goles
-	();
+	(id_equipo_casa int unsigned,
+    id_equipo_fuera int unsigned,
+    minuto int,
+    descripcion varchar(500) not null,
+    id_jugador int unsigned,
+	constraint ck_minuto check(minuto between 0 and 130),
+    constraint fk_id_jugador foreign key (id_jugador) references jugadores(id_jugador),
+    constraint fk_id_equipo foreign key (id_equipo_casa, id_equipo_fuera) references partidos(id_equipo_casa, id_equipo_fuera));
